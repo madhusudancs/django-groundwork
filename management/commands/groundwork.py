0bd238f91a3003701738e05c6baceedca2c9a0f0
Krisys <krishna.bmsce@gmail.com>
2011-03-05 20:10:57 +0530

    Tested by creating a simple project and it works pretty decent..
    What is taking time now:
    1. Adding the database details
    2. Adding the url config to urls.py
    3. Adding the two apps in settings -> installed apps

--- management/commands/groundwork.py
+++ management/commands/groundwork.py
@@ -4,109 +4,131 @@ import os
 
 class Scaffolder():
   
-  def __init__(self, app , model_instances):
-    self.app = app
-    self.model_instances = model_instances
-    self.PROJECT_ROOT = os.path.abspath( os.path.join(os.path.dirname(__file__) , "..", "..", ".."))
-    self.TEMPLATE_DIR = os.path.join ( self.PROJECT_ROOT , 'templates')
-    self.scaffold()
+    def __init__(self, app , model_instances):
+        self.app = app
+        self.model_instances = model_instances
+        self.PROJECT_ROOT = os.path.abspath( os.path.join(os.path.dirname(__file__) , "..", "..", ".."))
+        self.TEMPLATE_DIR = os.path.join ( self.PROJECT_ROOT , 'templates')
+        self.scaffold()
   
-  def scaffold(self):
-    self.generate_forms()
-    self.generate_urls()
-    self.generate_views()
-    self.generate_templates()
-  
+    def scaffold(self):
+        self.generate_forms()
+        self.generate_urls()
+        self.generate_views()
+        self.generate_templates()
+        self.display_messages()
 
 
-  def generate_urls(self):
-    
-    # All the libraries needed for the url config file
-    urls = URL_IMPORTS
+    def display_messages(self):
+        print "Configure your settings.py with the desired database and execute manage.py syncdb"
+        print "Add your new app to the list of installed apps in settings.py"
 
-    # Generate CRUD urls for each model
+    def generate_urls(self):
     
-    for model_instance in self.model_instances:
-      urls += URL_CRUD_CONFIG % {'model':model_instance._meta.object_name.lower(), 'modelClass': model_instance._meta.object_name } 
-    
-    # Previous line should be written in a better way
-    
-    urls += ")"
-    
-    # write to urls.py
-    urlpath = os.path.join (self.PROJECT_ROOT, self.app, 'urls.py')
-#    with open( urlpath, 'w') as f:
-#      f.write(urls)
-    print "URL Config has been written to %s. \n\nAdd the following line to %s" % (urlpath, os.path.join (self.PROJECT_ROOT, 'urls.py'))
-    print "(r'^%(app)s/', include('%(app)s.urls'))," % {'app': self.app }
+        # All the libraries needed for the url config file
+        urls = URL_IMPORTS
+
+        # Generate CRUD urls for each model
+
+        for model_instance in self.model_instances:
+            urls += URL_CRUD_CONFIG % {'model':model_instance._meta.object_name.lower(), 'modelClass': model_instance._meta.object_name } 
+
+        # Previous line should be written in a better way
+
+        urls += URL_END
+
+        # write to urls.py
+        urlpath = os.path.join (self.PROJECT_ROOT , self.app, 'urls.py')
+        f = open( urlpath , 'w')
+        f.write(urls)
+        f.close()
+        print "URL Config has been written to %s. \n\nAdd the following line to %s" % (urlpath, os.path.join (self.PROJECT_ROOT, 'urls.py'))
+        print "(r'^%(app)s/', include('%(app)s.urls'))," % {'app': self.app }
 
 
 
-  def generate_views(self):
-    # all the imports needed views
-    views_content = VIEWS_IMPORTS
-    
-    for model_instance in self.model_instances:
-      views_content += VIEWS_CREATE 
-      views_content += VIEWS_LIST
-      views_content += VIEWS_VIEW
-      views_content += VIEWS_UPDATE
+    def generate_views(self):
+        # all the imports needed views
+        views_content = VIEWS_IMPORTS
 
-      views_content = views_content %  {'model':model_instance._meta.object_name.lower(), 'modelClass': model_instance._meta.object_name, 'app': self.app } 
-    
-    print views_content
-    # write to views.py
-    viewspath = os.path.join (self.PROJECT_ROOT, self.app, 'views.py')
-#    with open( viewspath , 'w') as f:
-#      f.write(views_content)
-    print "Views written to %s" % (viewspath)
+        for model_instance in self.model_instances:
+            views_content += VIEWS_CREATE 
+            views_content += VIEWS_LIST
+            views_content += VIEWS_VIEW
+            views_content += VIEWS_UPDATE
+
+            views_content = views_content %  {'model':model_instance._meta.object_name.lower(), 'modelClass': model_instance._meta.object_name, 'app': self.app } 
+
+        # write to views.py
+        viewspath = os.path.join (self.PROJECT_ROOT, self.app, 'views.py')
+        f = open( viewspath, 'w')
+        f.write(views_content)
+        f.close()
+        
+        print "Views written to %s" % (viewspath)
 
     
     
     
-  def generate_templates(self):
-    os.mkdir( os.path.join(self.TEMPLATE_DIR, self.app ) )
-    
-    for model_instance in self.model_instances:
-      with  open(os.path.join( self.TEMPLATE_DIR, self.app, 'create_%s' % (model_instance._meta.object_name.lower()) ) ) as f:
-        f.write( TEMPLATES_CREATE  %  { 'modelClass' : model_instance._meta.object_name } )
-      with  open(os.path.join( self.TEMPLATE_DIR, self.app, 'list_%s' % (model_instance._meta.object_name.lower()) ) ) as f:
-        f.write( TEMPLATES_LIST  %  { 'modelClass' : model_instance._meta.object_name } )
-      with  open(os.path.join( self.TEMPLATE_DIR, self.app, 'edit_%s' % (model_instance._meta.object_name.lower()) ) ) as f:
-        f.write( TEMPLATES_EDIT  %  { 'modelClass' : model_instance._meta.object_name } )
-      with  open(os.path.join( self.TEMPLATE_DIR, self.app, 'view_%s' % (model_instance._meta.object_name.lower()) ) ) as f:
-        f.write( TEMPLATES_VIEW  %  { 'modelClass' : model_instance._meta.object_name,  'model' : model_instance._meta.object_name.lower()} )
+    def generate_templates(self):
+        template_dir = os.path.join(self.TEMPLATE_DIR, self.app )
+        if not os.path.exists(template_dir):
+            os.makedirs(template_dir)
+        
+        print "Generate base template? [Y/N]?"
+        yn = raw_input()
+        if yn.lower() == 'y':
+            f = open(os.path.join(self.TEMPLATE_DIR, 'base.html') , 'w')
+            f.write(TEMPLATES_BASE % {})
+            f.close()
+            
+        for model_instance in self.model_instances:
+            f = open(os.path.join( self.TEMPLATE_DIR, self.app, 'create_%s.html' % (model_instance._meta.object_name.lower()) ) ,'w')
+            f.write( TEMPLATES_CREATE  %  { 'modelClass' : model_instance._meta.object_name } )
+            f.close()
+            
+            f = open(os.path.join( self.TEMPLATE_DIR, self.app, 'list_%s.html' % (model_instance._meta.object_name.lower()) ) ,'w')
+            f.write( TEMPLATES_LIST  %  { 'modelClass' : model_instance._meta.object_name } )
+            f.close()
+            
+            f = open(os.path.join( self.TEMPLATE_DIR, self.app, 'edit_%s.html' % (model_instance._meta.object_name.lower()) ) ,'w') 
+            f.write( TEMPLATES_EDIT  %  { 'modelClass' : model_instance._meta.object_name } )
+            f.close()
+            
+            f = open(os.path.join( self.TEMPLATE_DIR, self.app, 'view_%s.html' % (model_instance._meta.object_name.lower()) ) , 'w')
+            f.write( TEMPLATES_VIEW  %  { 'modelClass' : model_instance._meta.object_name,  'model' : model_instance._meta.object_name.lower()} )
+            f.close()
 
       
-  def generate_forms(self):
-    
-    forms_content = FORMS_IMPORTS
-    for model_instance in self.model_instances:
-      forms_content += FORMS_MODELFORM_CONFIG % { 'modelClass' : model_instance._meta.object_name }
+    def generate_forms(self):
 
-    print forms_content
-    formspath = os.path.join (self.PROJECT_ROOT, self.app, 'forms.py')
-#    with open( formspath , 'w') as f:
-#      f.write(forms_content)
-    print "Forms written to %s" % (formspath)
+        forms_content = FORMS_IMPORTS
+        for model_instance in self.model_instances:
+            forms_content += FORMS_MODELFORM_CONFIG % { 'modelClass' : model_instance._meta.object_name }
+
+        formspath = os.path.join (self.PROJECT_ROOT, self.app, 'forms.py')
+        f = open( formspath , 'w')
+        f.write(forms_content)
+        f.close()
+        print "Forms written to %s" % (formspath)
     
     
 class Command(BaseCommand):
 
-  def handle(self, *args, **options):
-    "Usage : manage.py groundwork <app> <model>"
-    
-#    try:
-    app = args[0] # App name is the first parameter
-    
-    model_names = args[1:] # Models which need to be scaffolded will follow
-    
-    exec( "from %s.models import *" %(app) )
+    def handle(self, *args, **options):
+        "Usage : manage.py groundwork <app> <model>"
 
-    model_instances = [ eval(x) for x in model_names]
-    
-    Scaffolder(app, model_instances)
-  
-#    except:
-    print "Usage : manage.py groundwork <app> <model>"
+        try:
+            app = args[0] # App name is the first parameter
+
+            model_names = args[1:] # Models which need to be scaffolded will follow
+
+            exec( "from %s.models import *" %(app) )
+
+            model_instances = [ eval(x) for x in model_names]
+
+            Scaffolder(app, model_instances)
+
+        except:
+            print "Usage : manage.py groundwork <app> <model>"
 
--- management/commands/placeholders.py
+++ management/commands/placeholders.py
@@ -12,13 +12,15 @@ urlpatterns = patterns('',
 """
 
 URL_CRUD_CONFIG = """
-  (r'%(model)s/create/$', create_%(model)s),
-  (r'%(model)s/list/$', list_%(model)s ),
-  (r'%(model)s/edit/(?P<id>[^/]+)/$', edit_%(model)s),
-  (r'%(model)s/view/(?P<id>[^/]+)/$', view_%(model)s),
-  """ 
+    (r'%(model)s/create/$', create_%(model)s),
+    (r'%(model)s/list/$', list_%(model)s ),
+    (r'%(model)s/edit/(?P<id>[^/]+)/$', edit_%(model)s),
+    (r'%(model)s/view/(?P<id>[^/]+)/$', view_%(model)s),
+    """ 
 
-
+URL_END = """
+)
+"""
 
 
 
@@ -36,12 +38,12 @@ FORMS_MODELFORM_CONFIG = """
 
 class %(modelClass)sForm(forms.ModelForm):
 	
-  class Meta:
-    model = %(modelClass)s	
-    # exclude = [] # uncomment this line and specify any field to exclude it from the form
+    class Meta:
+        model = %(modelClass)s	
+        # exclude = [] # uncomment this line and specify any field to exclude it from the form
 
-  def __init__(self, *args, **kwargs):
-    super(%(modelClass)sForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(%(modelClass)sForm, self).__init__(*args, **kwargs)
 
 """		
 
@@ -71,14 +73,14 @@ from forms import *
 VIEWS_CREATE = """
 
 def create_%(model)s(request):
-  form = %(modelClass)sForm(request.POST or None)
-  if form.is_valid():
-    form.save()
-    form = %(modelClass)sForm()
-  
-  t = get_template('%(app)s/create_%(model)s.html')
-  c = RequestContext(request,locals())
-  return HttpResponse(t.render(c))
+    form = %(modelClass)sForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = %(modelClass)sForm()
+
+    t = get_template('%(app)s/create_%(model)s.html')
+    c = RequestContext(request,locals())
+    return HttpResponse(t.render(c))
 
 """
 
@@ -86,23 +88,23 @@ VIEWS_LIST = """
 
 def list_%(model)s(request):
   
-  list_items = %(modelClass)s.objects.all()
-  paginator = Paginator(list_items ,20)
+    list_items = %(modelClass)s.objects.all()
+    paginator = Paginator(list_items ,20)
 
 
-  try:
-    page = int(request.GET.get('page', '1'))
-  except ValueError:
-    page = 1
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
 
-  try:
-    list_items = paginator.page(page)
-  except (EmptyPage, InvalidPage):
-    list_items = paginator.page(paginator.num_pages)
+    try:
+        list_items = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        list_items = paginator.page(paginator.num_pages)
 
-  t = get_template('%(app)s/list_%(model)s.html')
-  c = RequestContext(request,locals())
-  return HttpResponse(t.render(c))
+    t = get_template('%(app)s/list_%(model)s.html')
+    c = RequestContext(request,locals())
+    return HttpResponse(t.render(c))
 
 """
 
@@ -110,26 +112,26 @@ def list_%(model)s(request):
 VIEWS_UPDATE = """
 def edit_%(model)s(request, id):
 
-  %(model)s_instance = %(modelClass)s.objects.get(id=id)
+    %(model)s_instance = %(modelClass)s.objects.get(id=id)
 
-  form = %(modelClass)sForm(request.POST or None, instance = %(model)s_instance)
+    form = %(modelClass)sForm(request.POST or None, instance = %(model)s_instance)
 
-  if form.is_valid():
-    form.save()
+    if form.is_valid():
+        form.save()
 
-  t=get_template('%(app)s/edit_%(model)s.html')
-  c=RequestContext(request,locals())
-  return HttpResponse(t.render(c))
+    t=get_template('%(app)s/edit_%(model)s.html')
+    c=RequestContext(request,locals())
+    return HttpResponse(t.render(c))
 """
 
 VIEWS_VIEW = """
 
 def view_%(model)s(request, id):
-  %(model)s_instance = %(modelClass)s.objects.get(id = id)
+    %(model)s_instance = %(modelClass)s.objects.get(id = id)
 
-  t=get_template('%(app)s/view_%(model)s.html')
-  c=RequestContext(request,locals())
-  return HttpResponse(t.render(c))
+    t=get_template('%(app)s/view_%(model)s.html')
+    c=RequestContext(request,locals())
+    return HttpResponse(t.render(c))
 """
 
 
@@ -159,8 +161,8 @@ TEMPLATES_LIST = """
 {%% block heading %%}  %(modelClass)s - List  {%% endblock %%}
 {%% block content %%} 
 <table>
-{%% for item in list_items %%}
-  <tr><td>forloop.counter</td><td> {{item}} </td></tr>
+{%% for item in list_items.object_list %%}
+  <tr><td>{{forloop.counter}}</td><td> {{item}} </td></tr>
 {%% endfor %%}
 </table>
 
@@ -208,6 +210,19 @@ TEMPLATES_VIEW = """
 <table>
 {{ %(model)s_instance }}
 </table>
+{%% endblock %%}
+"""
+
+TEMPLATES_BASE = """
+{%% block title %%} 
+{%% endblock %%}
+
+{%% block heading %%}  
+{%% endblock %%}
+
+{%% block content %%} 
+
+
 {%% endblock %%}
 """
 
